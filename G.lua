9e034cbfc08d8b8ee77dbca1ecbed47f9641b6b0
AIDE76f <stdre701mo@gmail.com>
2026-07-31 03:53:23 +0300

    Increase humanoid health from 100 to 1000

--- G.lua
+++ G.lua
@@ -2,5 +2,5 @@ local RunService = game:GetService("RunService")
 local humanoid = game.Players.LocalPlayer.Character.Humanoid
 
 RunService.Heartbeat:Connect(function()
-    humanoid.Health = 100
+    humanoid.Health = 1000
 end)
